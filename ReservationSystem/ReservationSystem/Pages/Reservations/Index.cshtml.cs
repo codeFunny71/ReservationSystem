6cf4babea53b493ef52c581b8584f99d308d2866
codeFunny71 <whynotlaugh@gmail.com>
2019-11-02 15:36:34 -0700

    added a search filter

--- ReservationSystem/ReservationSystem/Pages/Reservations/Index.cshtml.cs
+++ ReservationSystem/ReservationSystem/Pages/Reservations/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ReservationSystem.Data;
 using ReservationSystem.Models;
@@ -20,10 +21,22 @@ namespace ReservationSystem.Pages.Reservations
         }
 
         public IList<Reservation> Reservation { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        //// Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        //public SelectList Boats { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        //public string BoatType { get; set; }
 
         public async Task OnGetAsync()
         {
-            Reservation = await _context.Reservation.ToListAsync();
+            var reservations = from m in _context.Reservation
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                reservations = reservations.Where(s => s.Name.Contains(SearchString));
+            }
+            Reservation = await reservations.ToListAsync();
         }
     }
 }
